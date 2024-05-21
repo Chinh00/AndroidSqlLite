f47b8f812e461240251bd52d0d3ffdfd06f127a7
Ching <94354399+Chinh00@users.noreply.github.com>
2024-05-21 08:27:00 +0700

    first commit

--- app/src/main/java/com/superman/androidsqllite/MainActivity.kt
+++ app/src/main/java/com/superman/androidsqllite/MainActivity.kt
@@ -1,29 +1,164 @@
 package com.superman.androidsqllite
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import com.superman.androidsqllite.ui.theme.AndroidSqlLiteTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.superman.androidsqllite.database.DatabaseInstance
 
 class MainActivity : ComponentActivity() {
+    private val DatabaseInstance = DatabaseInstance(this);
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        var malop = ""
+        var tenlop = ""
+        var siso = 0
+
+
+
         setContent {
-            AndroidSqlLiteTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                MainScreen(onCreate = {
+                    val dbContext = DatabaseInstance.writableDatabase
+                    dbContext.beginTransaction()
+                    val contentValues = ContentValues().apply {
+                        put(com.superman.androidsqllite.database.DatabaseInstance.MA_LOP, it.maLop)
+                        put(com.superman.androidsqllite.database.DatabaseInstance.TEN_LOP, tenlop)
+                        put(com.superman.androidsqllite.database.DatabaseInstance.SI_SO, it.siSo)
+                    }
+                    dbContext.insert(com.superman.androidsqllite.database.DatabaseInstance.TABLE_NAME, null, contentValues)
+                    dbContext.setTransactionSuccessful()
+                    Toast.makeText(this, "Thêm mới thành công", Toast.LENGTH_LONG).show()
+
+
+
+                }, onUpdate = {}, onDelete = {}, onQuery = {})
+
+            }
+        }
+    }
+    fun Validate(lop: Lop) = {
+    }
+
+}
+
+
+
+data class Lop(val maLop: String, val tenLop: String, val siSo: Int)
+
+@Preview
+@Composable
+fun MainScreen(onCreate: (Lop) -> Unit, onUpdate: (Lop) -> Unit, onDelete: (Lop) -> Unit, onQuery: () -> Unit) {
+    var malop by remember { mutableStateOf("") }
+    var tenlop by remember { mutableStateOf("") }
+    var siso by remember { mutableStateOf("") }
+
+
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Greeting("QUẢN LÝ SINH VIÊN")
+        Column(modifier = Modifier.padding(20.dp)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Mã lớp", modifier = Modifier.width(100.dp), fontSize = 20.sp)
+                TextField(
+                    value = malop,
+                    onValueChange = {
+                        malop = it
+                    },
+                    textStyle = TextStyle(fontSize = 18.sp),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Tên lớp", modifier = Modifier.width(100.dp), fontSize = 20.sp)
+                TextField(
+                    value = tenlop,
+                    onValueChange = {
+                        tenlop = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    textStyle = TextStyle(fontSize = 18.sp),
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Sĩ số", modifier = Modifier.width(100.dp), fontSize = 20.sp)
+                TextField(
+                    value = siso,
+                    onValueChange = {
+                        siso = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    textStyle = TextStyle(fontSize = 18.sp),
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row (modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = {
+                                 onCreate(Lop(malop, tenlop, siSo = siso.toInt()))
+                }, modifier = Modifier.weight(1f)) {
+                    Text(text = "INSERT")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { onDelete(Lop(malop, tenlop, siSo = siso.toInt())) }, modifier = Modifier.weight(1f)) {
+                    Text(text = "DELETE")
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row {
+                Button(onClick = { onUpdate(Lop(malop, tenlop, siSo = siso.toInt())) }, modifier = Modifier.weight(1f)) {
+                    Text(text = "UPDATE")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { onQuery() }, modifier = Modifier.weight(1f)) {
+                    Text(text = "QUERY")
                 }
             }
         }
@@ -41,7 +176,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AndroidSqlLiteTheme {
-        Greeting("Android")
-    }
+
 }
--- app/src/main/java/com/superman/androidsqllite/database/DatabaseInstance.java
+++ app/src/main/java/com/superman/androidsqllite/database/DatabaseInstance.java
@@ -7,16 +7,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseInstance extends SQLiteOpenHelper {
+    public static final String MA_LOP = "MA_LOP";
+    public static final String TEN_LOP = "TEN_LOP";
+    public static final String SI_SO = "SI_SO";
+    public static final String TABLE_NAME = "TBL_LOP";
+
+    private static final String DATABASE_NAME = "lop.db";
 
 
+    public DatabaseInstance(Context context) {
+        super(context, DATABASE_NAME, null, 1);
 
-    public DatabaseInstance(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE TBL_LOP (MA_LOP VARCHAR(10) PRIMARY KEY, TEN_LOP NVARCHAR(100), SI_SO int)");
     }
 
     @Override
